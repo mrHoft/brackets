9d1cc05a65505a0620b33386843a436d9034d41c
mrHoft <ru.hoft@gmail.com>
2023-08-08 17:45:51 +0300

    feat: check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, conf) {
+  const out = [];
+  const arr = str.split("");
+  for (const b of arr) {
+    if (conf.some((v) => v[1] === b && out.slice(-1)[0] === v[0])) out.pop();
+    else if (conf.some((v) => v[0] === b)) out.push(b);
+    else return false;
+    // console.log(b, out.join(""));
+  }
+  return !out.length;
 }
+
+module.exports = check;
